234e7208024f32ae632a76ed4dc24698ce04407e
Narvak <91684269+Narvak@users.noreply.github.com>
2022-02-25 10:09:01 +0100

    Add files via upload

--- javascript/all-lib_js.js
+++ javascript/all-lib_js.js
@@ -2,3 +2,9 @@ function dmode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+function img_header() {
+    var element = document.header;
+    element.classList.toggle("images");
+        function alert(message?: any): void
+}
